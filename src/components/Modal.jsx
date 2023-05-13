291674d3608cc9ed0aa40160781b2577b9d10a09
etopritika <dimaprytyka@gmail.com>
2023-05-13 14:00:44 +0300

    init

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,16 +6,18 @@ const modalRoot = document.querySelector('#modal-root');
 
 export default class Modal extends Component {
   componentDidMount() {
-    window.addEventListener('keydown', e => {
-      console.log(e.code);
-      if (e.code === 'Escape') {
-        this.props.onClose();
-      }
-    });
+    window.addEventListener('keydown', this.handleKeyDown);
   }
   componentWillUnmount() {
-    window.removeEventListener('keydown', this.props.onClose);
+    window.removeEventListener('keydown', this.handleKeyDown);
   }
+
+  handleKeyDown = e => {
+    console.log(e.code);
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
 
   render() {
     return createPortal(
